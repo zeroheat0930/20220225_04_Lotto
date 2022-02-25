ebda079e0766b6f1bfd998519a69a460576b587e
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-02-25 12:11:59 +0900

    하나의 스캐너로 숫자 6번 입력받기

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,9 +1,21 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 	public static void main(String[] args) {
 
-		System.out.println("Hello world");
+//	 6개의 숫자 입력받기.
+		
+		Scanner S = new Scanner(System.in);
+		
+		for(int i = 0 ; i < 6; i++) {
+			
+			System.out.println((i+1) + "번째 숫자 : ");
+			
+		int inputNum = S.nextInt();
+			
+		}
 		
 	}
 }
